57695c20fcb11bab42451aad1cc80e1eb004a30e
Xantras44 <santi.r.s4@gmail.com>
2020-04-19 18:26:22 +0200

    Empezada la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BarraNavComponent } from './barra-nav/barra-nav.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'barra-nav', component: BarraNavComponent, pathMatch: 'full'},
+  { path: 'login', component: LoginComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BarraNavComponent } from './barra-nav/barra-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BarraNavComponent
   ],
   imports: [
     BrowserModule,
