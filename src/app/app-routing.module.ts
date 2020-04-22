53e5ff15e27a8dfe5eb254c656cc772e257658ab
Xantras44 <santi.r.s4@gmail.com>
2020-04-22 21:08:40 +0200

    Retoques.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { SubirProductoComponent } from './inside/subir_producto/subir_producto.c
 import { VerProductoComponent } from './inside/ver_producto/ver_producto.component';
 import { HomeComponent } from './inside/home/home.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { CuerpoComponent } from './presentacion/cuerpo/cuerpo.component';
 import { RegistroComponent } from './presentacion/registro/registro.component';
 import { InicioSesionComponent } from './presentacion/inicio-sesion/inicio-sesion.component';
 import { InsideComponent } from './inside/inside.component';
@@ -13,10 +14,11 @@ const routes: Routes = [
   { path: 'ver', component: VerProductoComponent },
   { path: 'home', component: HomeComponent },
   { path: 'presentacion', component: PresentacionComponent },
+  { path: 'cuerpo', component: CuerpoComponent},
   { path: 'registro', component: RegistroComponent },
   { path: 'InicioSesion', component: InicioSesionComponent },
   { path: 'inside', component: InsideComponent },
-  { path:'', component: PresentacionComponent,pathMatch:'full'},
+  { path: '', component: PresentacionComponent,pathMatch:'full'},
   { path:'**',redirectTo:'/',pathMatch:'full'} /*redirigir a una pagina error 404 (crear otro componente)*/
 ];
 
