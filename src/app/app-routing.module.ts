ee392891735d36427954bbbd87f40237397fad9b
Jorge <100383398@alumnos.uc3m.es>
2020-04-22 23:03:57 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { SubirProductoComponent } from './inside/subir_producto/subir_producto.c
 import { VerProductoComponent } from './inside/ver_producto/ver_producto.component';
 import { HomeComponent } from './inside/home/home.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
-import { RegistroComponent } from './presentacion/registro/registro.component';
-import { InicioSesionComponent } from './presentacion/inicio-sesion/inicio-sesion.component';
-import { InsideComponent } from './inside/inside.component';
+import { RegistroComponent } from './registro/registro.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+
 
 const routes: Routes = [
   { path: 'subida', component: SubirProductoComponent },
@@ -15,9 +15,8 @@ const routes: Routes = [
   { path: 'presentacion', component: PresentacionComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'InicioSesion', component: InicioSesionComponent },
-  { path: 'inside', component: InsideComponent },
-  { path:'', component: PresentacionComponent,pathMatch:'full'},
-  { path:'**',redirectTo:'/',pathMatch:'full'} /*redirigir a una pagina error 404 (crear otro componente)*/
+  {path:'', component: PresentacionComponent,pathMatch:'full'},
+  {path:'**',redirectTo:'/',pathMatch:'full'} /*redirigir a una pagina error 404 (crear otro componente)*/
 ];
 
 @NgModule({
