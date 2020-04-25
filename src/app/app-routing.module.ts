a116c78ebbf4d22864999429f3b0881726212879
Xantras44 <santi.r.s4@gmail.com>
2020-04-25 13:16:45 +0200

    Footer fuera y RouterLinks incluidos en la presentación.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { SubirProductoComponent } from './inside/subir_producto/subir_producto.c
 import { VerProductoComponent } from './inside/ver_producto/ver_producto.component';
 import { HomeComponent } from './inside/home/home.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
-import { CuerpoComponent } from './presentacion/cuerpo/cuerpo.component';
 import { RegistroComponent } from './presentacion/registro/registro.component';
 import { InicioSesionComponent } from './presentacion/inicio-sesion/inicio-sesion.component';
 import { ConocerComponent } from './inside/conocer/conocer.component';
@@ -14,10 +13,9 @@ const routes: Routes = [
   { path: 'ver', component: VerProductoComponent },
   { path: 'home', component: HomeComponent },
   { path: 'presentacion', component: PresentacionComponent },
-  { path: 'cuerpo', component: CuerpoComponent},
   { path: 'registro', component: RegistroComponent },
-  { path: 'InicioSesion', component: InicioSesionComponent },
-  { path: 'conocer', component: ConocerComponent},
+  { path: 'inicio-sesion', component: InicioSesionComponent },
+  { path: 'conocenos', component: ConocerComponent},
   { path: '', component: PresentacionComponent,pathMatch:'full'},
   { path:'**',redirectTo:'/',pathMatch:'full'} /*redirigir a una pagina error 404 (crear otro componente)*/
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './inside/encabezado/encabezado.component';
-import { FooterComponent } from './inside/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 import { SubirProductoComponent } from './inside/subir_producto/subir_producto.component';
 import { VerProductoComponent } from './inside/ver_producto/ver_producto.component';
 import { HomeComponent } from './inside/home/home.component';
@@ -13,7 +13,6 @@ import { RegistroComponent } from './presentacion/registro/registro.component';
 import { InicioSesionComponent } from './presentacion/inicio-sesion/inicio-sesion.component';
 import { EncabezadoPrincipalComponent } from './presentacion/encabezado-principal/encabezado-principal.component';
 import { FooterPrincipalComponent } from './presentacion/footer-principal/footer-principal.component';
-import { CuerpoComponent } from './presentacion/cuerpo/cuerpo.component';
 import { ConocerComponent } from './inside/conocer/conocer.component';
 
 @NgModule({
@@ -29,7 +28,6 @@ import { ConocerComponent } from './inside/conocer/conocer.component';
     InicioSesionComponent,
     EncabezadoPrincipalComponent,
     FooterPrincipalComponent,
-    CuerpoComponent,
     ConocerComponent
   ],
   imports: [
