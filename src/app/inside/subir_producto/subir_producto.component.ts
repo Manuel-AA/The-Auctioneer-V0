05b4cda18b41e5d7607e1689abc361ed04afb472
Xantras44 <santi.r.s4@gmail.com>
2020-04-27 22:33:30 +0200

    Se pueden subir productos.

--- src/app/inside/subir_producto/subir_producto.component.ts
+++ src/app/inside/subir_producto/subir_producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../../services/firestore.service';
 
 @Component({
   selector: 'app-subir_producto',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubirProductoComponent implements OnInit {
 
-  constructor() { }
+  producto: any = {
+    nombre: "",
+    precioSalida: "",
+    pujaActual: 0,
+    precioCompraYa: ""
+  }
+
+  constructor( private servicio: FirestoreService) { }
 
   ngOnInit() {
+  }
+
+  AgregarProducto() {
+    this.producto.precioCompraYa = this.producto.precioSalida * 1.5;
+    this.servicio.addProducto(this.producto);
+    this.producto.nombre = "";
+    this.producto.precioSalida = "";
+    this.producto.pujaActual = 0;
+    this.producto.precioCompraYa = "";
   }
 
 }
