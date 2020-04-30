e6694506a4ff11c2e0b50494a3677de5d439d3ef
Xantras44 <santi.r.s4@gmail.com>
2020-04-30 20:16:04 +0200

    Fuera el error del buscador

--- src/app/filtro/filtro.pipe.ts
+++ src/app/filtro/filtro.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FiltroPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
+  transform(value: any = "", ...args: any[]): any {
     const resultadoBusqueda = [];
     for (const producto of value) {
       if (producto.nombre.indexOf(args) > -1) {
